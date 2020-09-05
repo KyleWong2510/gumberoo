49c1e2afc8dd519e8ab1446bce29d44359752cbf
Bill Wilke <billwilke2@gmail.com>
2020-09-05 17:17:50 -0600

    Adds actions for reducers created, and a mock data folder with a lesson variable

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,24 @@
+export const setLesson = lesson => ({
+  type: 'SET_LESSON',
+  lesson
+})
+
 
 export const setStudentName = studentName => ({
   type: 'SET_STUDENT_NAME',
   studentName
+})
+
+export const hasErrored = message => ({
+  type: 'HAS_ERRORED',
+  message
+})
+
+export const reset = () => ({
+  type: 'RESET'
+})
+
+export const isLoading = bool => ({
+  type: 'IS_LOADING',
+  isLoading: bool
 })
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux'
 import { setStudentName } from './setStudentName'
+import { setLesson } from './setLesson'
+import { isLoading } from './isLoading'
+import { hasErrored } from './hasErrored' 
 
 export const rootReducer = combineReducers({
-  setStudentName
+  setStudentName,
+  isLoading,
+  hasErrored,
+  setLesson
 })
