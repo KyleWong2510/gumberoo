4a77149adb743d14680f9411483cc4fcb7d0046b
Kyle Wong <kylewong2510@gmail.com>
2020-09-10 11:23:08 -0600

    Style component titles for teacher views

--- src/CreateLesson/CreateLesson.js
+++ src/CreateLesson/CreateLesson.js
@@ -13,19 +13,22 @@ const CreateLesson = () => {
 
   return (
     <section className='create-lesson'>
-      <QuestionFormCard 
-        lessonTitleText={lessonTitleText} 
-        setLessonTitleText={setLessonTitleText} 
-        questions={questions} 
-        setQuestions={setQuestions}
-      />
-      <LessonPreview 
-        lessonTitleText={lessonTitleText} 
-        setLessonTitleText={setLessonTitleText}
-        questions={questions}
-        setQuestions={setQuestions}
-        deleteQuestion={deleteQuestion}
-      />
+      <h1>Create a Lesson</h1>
+      <section className='create-lesson-body'>
+        <QuestionFormCard 
+          lessonTitleText={lessonTitleText} 
+          setLessonTitleText={setLessonTitleText} 
+          questions={questions} 
+          setQuestions={setQuestions}
+        />
+        <LessonPreview 
+          lessonTitleText={lessonTitleText} 
+          setLessonTitleText={setLessonTitleText}
+          questions={questions}
+          setQuestions={setQuestions}
+          deleteQuestion={deleteQuestion}
+        />
+      </section>
     </section>
   )
 }
