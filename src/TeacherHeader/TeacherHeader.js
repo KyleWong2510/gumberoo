f1b7560cae2e1f3949fca66cc1bf4169e913e322
Kyle Wong <kylewong2510@gmail.com>
2020-09-10 12:07:33 -0600

    Fix build errors

--- src/TeacherHeader/TeacherHeader.js
+++ src/TeacherHeader/TeacherHeader.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './TeacherHeader.scss'
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const TeacherHeader = () => {
   return (
