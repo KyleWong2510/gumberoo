53f50fee56488cc4868fa54c02ceb8a76747302a
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-09-11 13:33:52 -0600

    initial commit

--- src/TeacherLessons/TeacherLessons.js
+++ src/TeacherLessons/TeacherLessons.js
@@ -2,6 +2,16 @@ import React from 'react'
 import './TeacherLessons.scss'
 
 const TeacherLessons = () => {
+
+// Upon load of test Teacher:
+// a GET will occur to grab the existing lessons saved for a teacher. 
+// whenever a new lesson is created - it is saved to the store and will
+// prompt a rerender on this page as we list out the lessons
+// Lesson Title <- clickable for stats on the lesson
+// Lesson Link
+
+
+
   return (
     <main className='teacher-lessons'>
       <h1>Lessons</h1>
@@ -9,5 +19,8 @@ const TeacherLessons = () => {
     </main>
   )
 }
+
+
+
 
 export default TeacherLessons
