795705df0119097bce235d7f27df2d4e121290f7
Kyle Wong <kylewong2510@gmail.com>
2020-09-13 11:08:19 -0600

    Pass setLesson reducer tests

--- src/reducers/setLesson.test.js
+++ src/reducers/setLesson.test.js
@@ -1,9 +1,27 @@
 import { setLesson } from './setLesson'
+import lesson from '../mockData/mockData'
 
 describe('setLessons', () => {
   it('Should return an initial state', () => {
     const expected = {}
     const results = setLesson(undefined, {})
     expect(results).toEqual(expected)
+  })
+
+  it('Should set lessons', () => {
+    const expected = lesson
+    const results = setLesson(undefined, {
+      type: 'SET_LESSON',
+      lesson
+    })
+    expect(results).toEqual(expected)
+  })
+
+  it('Should reset lessons', () => {
+    const expected = {}
+    const result = setLesson(undefined, {
+      type: 'RESET'
+    })
+    expect(result).toEqual(expected)
   })
 })
