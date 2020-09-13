b174918b20d25b2231afb48e07f05ec7c890b060
Kyle Wong <kylewong2510@gmail.com>
2020-09-13 11:33:04 -0600

    Pass tests for setStudents reducers

--- src/reducers/setStudents.test.js
+++ src/reducers/setStudents.test.js
@@ -1,9 +1,40 @@
 import { setStudents } from './setStudents'
+import student1 from '../mockData/mockData'
+import student2 from '../mockData/mockData'
+import student3 from '../mockData/mockData'
+
+const students = [student1, student2]
 
 describe('setStudents', () => {
   it('Should return an initial state', () => {
     const expected = []
     const results = setStudents(undefined, [])
     expect(results).toEqual(expected)
+  })
+
+  it('Should set students', () => {
+    const expected = students
+    const results = setStudents(undefined, {
+      type: 'SET_STUDENTS',
+      students
+    })
+    expect(results).toEqual(expected)
+  })
+
+  it('Should add a student', () => {
+    const expected = [student1, student2, student3]
+    const results = setStudents(students, {
+      type: 'ADD_STUDENT',
+      students: student3
+    })
+    expect(results).toEqual(expected)
+  })
+
+  it('Should reset students', () => {
+    const expected = []
+    const result = setStudents(undefined, {
+      type: 'RESET'
+    })
+    expect(result).toEqual(expected)
   })
 })
