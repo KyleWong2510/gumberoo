3ac8dfab7378a41b71b57c17a0d49d0f219dc5e1
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-09-15 13:51:03 -0600

    removed link from LessonCard

--- src/TeacherLessons/LessonCard/LessonCard.js
+++ src/TeacherLessons/LessonCard/LessonCard.js
@@ -2,13 +2,11 @@ import React from 'react'
 import './LessonCard.scss'
 import PropTypes from 'prop-types'
 
-const LessonCard = ({ id, lessonTitle, lessonLink, findLesson, deleteLesson})=> {
+const LessonCard = ({ id, lessonTitle, findLesson})=> {
 
   return (
     <section id={id} key={id} className='lesson-card'>
       <p onClick={(e) => findLesson(e)}> {lessonTitle}</p>
-      <p><span className='question-card-category'>Lesson Link:</span> {lessonLink}</p>
-      {/* <button onClick={(e) => deleteLesson(e)}>Delete</button> */}
     </section>
   )
 }
